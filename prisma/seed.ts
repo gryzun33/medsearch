376241eab39f823aefd3090a0a4a8199ebab62eb
gryzun33 <yakushevaolga564@gmail.com>
2025-03-15 08:44:11 +0300

    feat: add seeds of pharmacies

---
+++ prisma/seed.ts
@@ -0,0 +1,33 @@
+import { PrismaClient } from '@prisma/client';
+import pharmacies from './data/pharmacies.json';
+// const pharmacies = require('./data/pharmacies.json');
+console.log('pharmacies=', pharmacies);
+
+const prisma = new PrismaClient();
+
+async function main() {
+  console.log('Seeding database...');
+
+  for (const pharmacy of pharmacies) {
+    await prisma.pharmacy.create({
+      data: {
+        name: pharmacy.name,
+        address: pharmacy.address,
+        latitude: pharmacy.position[0],
+        longitude: pharmacy.position[1],
+        hours: pharmacy.hours,
+      },
+    });
+  }
+
+  console.log('Seeding completed.');
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
