9336d7677c38c5616c73a3e9cc1ccba1673aaa8d
gryzun33 <yakushevaolga564@gmail.com>
2025-03-17 16:48:29 +0300

    feat: change format of pharmacy

--- src/pharmacy/pharmacy.controller.ts
+++ src/pharmacy/pharmacy.controller.ts
@@ -1,17 +1,18 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { PharmacyService } from './pharmacy.service';
+import { Pharmacy } from './entities/pharmacy.entity';
 
 @Controller('pharmacy')
 export class PharmacyController {
   constructor(private readonly pharmacyService: PharmacyService) {}
 
   @Get()
-  async getAll() {
+  async getAll(): Promise<Pharmacy[] | null> {
     return this.pharmacyService.getAll();
   }
 
   @Get(':id')
-  async getById(@Param('id') id: string) {
+  async getById(@Param('id') id: string): Promise<Pharmacy | null> {
     return this.pharmacyService.getById(id);
   }
 }
--- src/pharmacy/pharmacy.service.ts
+++ src/pharmacy/pharmacy.service.ts
@@ -1,15 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/common/prisma/prisma.service';
+import { Pharmacy } from '@prisma/client';
+import { Pharmacy as PharmacyEntity } from './entities/pharmacy.entity';
 
 @Injectable()
 export class PharmacyService {
   constructor(private prisma: PrismaService) {}
 
-  async getAll() {
-    return this.prisma.pharmacy.findMany();
+  async getAll(): Promise<PharmacyEntity[] | null> {
+    const pharmacies = await this.prisma.pharmacy.findMany();
+    return pharmacies.map((item: Pharmacy) => ({
+      id: item.id,
+      name: item.name,
+      address: item.address,
+      position: [item.latitude, item.longitude],
+      hours: item.hours,
+    }));
   }
 
-  async getById(id: string) {
-    return this.prisma.pharmacy.findUnique({ where: { id } });
+  async getById(id: string): Promise<PharmacyEntity | null> {
+    const pharmacy = await this.prisma.pharmacy.findUnique({ where: { id } });
+
+    if (!pharmacy) {
+      throw new NotFoundException(`Pharmacy not found`);
+    }
+
+    return {
+      id: pharmacy.id,
+      name: pharmacy.name,
+      address: pharmacy.address,
+      position: [pharmacy.latitude, pharmacy.longitude],
+      hours: pharmacy.hours,
+    };
   }
 }
