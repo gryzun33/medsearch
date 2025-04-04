a65baffa7c527b4f56d10bf56405a5261089f4eb
gryzun33 <yakushevaolga564@gmail.com>
2025-04-04 12:10:44 +0300

    feat: add configs

---
+++ README.md
@@ -0,0 +1,68 @@
+# Medsearch
+
+## Downloading
+
+Clone the project
+
+```
+git clone https://github.com/gryzun33/medsearch.git
+```
+
+Go to folder with project
+
+```
+cd medsearch
+```
+
+Check if you are in branch `develop`
+
+## Installing NPM modules
+
+```
+npm install
+
+```
+
+## Preparing to run
+
+In folder `server` rename file `.env.example` to `.env`
+
+To create database (apply prisma migrations and add seeds)
+
+```
+npm run migrate:seed
+```
+
+## Running the project
+
+To run project in development mode, it's recommended to run frontend and backend in separate terminals:
+
+To run frontend:
+
+```
+npm run frontend
+```
+
+To run backend:
+
+```
+npm run server
+```
+
+To run frontend and backend in one terminal in development mode:
+
+```
+npm run dev
+```
+
+To build the project:
+
+```
+npm run build
+```
+
+To run project in production mode:
+
+```
+npm run start
+```
--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import pharmacies from './data/pharmacies.json';
 import medicines from './data/medicines.json';
-// console.log('pharmacies=', pharmacies);
 
 const prisma = new PrismaClient();
 
@@ -28,23 +27,29 @@ async function main() {
   const allMedicines = await prisma.medicine.findMany();
 
   for (const pharmacy of allPharmacies) {
-    // Определяем случайное количество связей для каждой аптеки
     const randomMedicines = allMedicines
-      .sort(() => 0.5 - Math.random()) // случайный порядок
-      .slice(0, Math.floor(Math.random() * allMedicines.length) + 1); // случайное количество
-
+      .sort(() => 0.5 - Math.random())
+      .slice(0, Math.floor(Math.random() * allMedicines.length) + 1);
     for (const medicine of randomMedicines) {
-      await prisma.stock.create({
-        data: {
+      const existingStock = await prisma.stock.findFirst({
+        where: {
           pharmacyId: pharmacy.id,
           medicineId: medicine.id,
-          price: parseFloat((Math.random() * (100 - 5) + 5).toFixed(2)), // случайная цена
-          quantity: Math.floor(Math.random() * 50) + 1, // случайное количество
         },
       });
+
+      if (!existingStock) {
+        await prisma.stock.create({
+          data: {
+            pharmacyId: pharmacy.id,
+            medicineId: medicine.id,
+            price: parseFloat((Math.random() * (100 - 5) + 5).toFixed(2)),
+            quantity: Math.floor(Math.random() * 50) + 1,
+          },
+        });
+      }
     }
   }
-
   console.log('Many-to-many links generated.');
 
   console.log('Seeding completed.');
